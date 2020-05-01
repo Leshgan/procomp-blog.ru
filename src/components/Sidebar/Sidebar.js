039da38db0969e108fcdcc631d0858782a39a294
Alex <leshgans@gmail.com>
2020-05-02 01:16:23 +0300

    tags in sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,34 @@
 // @flow strict
 import React from 'react';
-import Author from './Author';
+import { Link } from 'gatsby';
+import kebabCase from 'lodash/kebabCase';
+// import Author from './Author';
 import Contacts from './Contacts';
 import Copyright from './Copyright';
 import Menu from './Menu';
 import styles from './Sidebar.module.scss';
-import { useSiteMetadata } from '../../hooks';
+import { useSiteMetadata, useTagsList } from '../../hooks';
 
 type Props = {
   isIndex?: boolean,
+};
+
+const renderTags = () => {
+  const tags = useTagsList();
+  return (
+    <aside>
+      <h5>Метки</h5>
+      <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
+        {tags.map((tag) => (
+          <li key={tag.fieldValue} style={{ display: 'inline-block', padding: 0, margin: 0 }}>
+            <Link to={`/tag/${kebabCase(tag.fieldValue)}/`} style={{ fontSize: `${tag.totalCount + 9}pt`, marginRight: 3 }}>
+              {tag.fieldValue}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 };
 
 const Sidebar = ({ isIndex }: Props) => {
@@ -19,7 +39,8 @@ const Sidebar = ({ isIndex }: Props) => {
       <div className={styles['sidebar__inner']}>
         {/* <Author author={author} isIndex={isIndex} /> */}
         <Menu menu={menu} />
-        <Contacts contacts={author.contacts} />
+        {/* <Contacts contacts={author.contacts} /> */}
+        {renderTags()}
         <Copyright copyright={copyright} />
       </div>
     </div>
